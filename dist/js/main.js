9ecd5f324cf471ff888b644949cb150be9ce709a
Christopher Beards <beardscm@gmail.com>
2018-08-25 21:31:27 -0400

    test

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('../../sw_cached_site.js')
+      .register('/portfolio/sw_cached_site.js')
       .then(reg => console.log('Service Worker: registered'))
       .catch(err => console.log(err));
   });
